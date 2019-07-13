3d548ab5368d0c41abe12bacdaf614ee90cdb3bc
tkonstantin <k.tkachuk@consult-info.ru>
2019-07-13 19:00:26 +0200

    Git init

---
+++ local/modules/kanbanboard/lang/ru/install/index.php
@@ -0,0 +1,14 @@
+<?
+$MESS["CONSULTINFO_KANBANBOARD_MODULE_NAME"] = "Канбан доска";
+$MESS["CONSULTINFO_KANBANBOARD_MODULE_DESC"] = "Модуль добавляет канбан доску для организации задач на портале.";
+$MESS["CONSULTINFO_KANBANBOARD_PARTNER_NAME"] = "Consult-info";
+$MESS["CONSULTINFO_KANBANBOARD_PARTNER_URI"] = "http://consult-info.ru";
+$MESS["CONSULTINFO_KANBANBOARD_INSTALL"] = "Установка модуля \"Канбан доска\"";
+$MESS["CONSULTINFO_KANBANBOARD_UNINSTALL"] = "Деинсталяция модуля \"Канбан доска\"";
+$MESS["CONSULTINFO_KANBANBOARD_INSTALL_ERROR_VERSION"] = "Версия главного модуля ниже 14. Не поддерживается технология D7. Пожалуйста обновите систему.";
+$MESS["CONSULTINFO_KANBANBOARD_IBLOCK_TYPE_NAME_EN"] = "consultinfo.kanbanboard";
+$MESS["CONSULTINFO_KANBANBOARD_IBLOCK_TYPE_NAME_RU"] = "Канбан доска";
+$MESS["CONSULTINFO_KANBANBOARD_IBLOCK_TYPE_ALREADY_EXISTS"] = "Такой тип инфоблока уже существует";
+$MESS["CONSULTINFO_KANBANBOARD_IBLOCK_ALREADY_EXISTS"] = "Инфоблок с таким кодом уже существует";
+$MESS["CONSULTINFO_KANBANBOARD_IBLOCK_TYPE_DELETION_ERROR"] = "Ошибка удаления типа инфоблока";
+?>
